8c723ff68b585f3ce33fce9db72ed240e00294c6
drmodun <jan.modun.st@gmail.com>
2023-07-03 17:13:29 +0200

    improved valdiators a bit

---
+++ Domain/Validatiors/CompanyValidator.cs
@@ -0,0 +1,29 @@
+﻿using Data.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Domain.Validatiors
+{
+    public class CompanyValidator : AbstractValidator<Company>
+    {
+        public CompanyValidator()
+        {
+            var websiteCheck = new Regex(@"[-a-zA-Z0-9@:%._\+~#=]{1,256}\
+            .[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)");
+            var base64Check = new Regex(@"^(?:[A-Za-z0-9+/]{4})*
+            (?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$");
+
+            RuleFor(x => x.Name).Must(x => x.Length <= 50 && x.Length > 4).WithMessage("Name of category must be between 5 and 50 chaarcters");
+            RuleFor(x => x.Description).Must(x => x.Length <= 100 && x.Length > 10).WithMessage("Descciption of category must be between 10 and 100 chaarcters");
+            RuleFor(x => x.Website).Must(x => { return websiteCheck.IsMatch(x); });
+            RuleFor(x => x.Logo).Must(x => base64Check.IsMatch(x));
+        }
+
+    }
+}
---
+++ Domain/Validatiors/ServiceValidator.cs
@@ -0,0 +1,21 @@
+﻿using Data.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validatiors
+{
+    public class ServiceValidator : AbstractValidator<Service>
+    {
+        public ServiceValidator() 
+        {
+            RuleFor(x => x.Name).Must(x => x.Length <= 50 && x.Length > 4).WithMessage("Name of category must be between 5 and 50 chaarcters");
+            RuleFor(x => x.Description).Must(x => x.Length <= 100 && x.Length > 10).WithMessage("Descciption of category must be between 10 and 100 chaarcters");
+            RuleFor(x => x.Price).Must(x => x >= 1 && x <= 100000).WithMessage("Price must be reasonable");
+        }
+
+    }
+}
