b6a3fb69ec8c02eb169627f6bef62f27bd6ceb99
drmodun <jan.modun.st@gmail.com>
2023-07-10 15:32:03 +0200

    added react files, currently ony the basic files but will soon start writing frontend

---
+++ Web/app/src/Types/Types.d.ts
@@ -0,0 +1,18 @@
+        declare module "*.jpg" {
+    const path: string;
+    export default path;
+}
+
+ declare module "*.png" {
+    const path: string;
+    export default path;
+}
+
+declare module "*.svg" {
+    const path: string;
+    export default path;
+}
+declare module "*.scss" {
+    const path: string;
+    export default path;
+}
