6303fa6090c3becf87c11a112c047ddb06689527
drmodun <jan.modun.st@gmail.com>
2023-07-19 12:54:36 +0200

    added basic Itemview with possbible links

---
+++ Web/app/src/Components/Admin/Forms/index.ts
@@ -0,0 +1,17 @@
+import { SubcategoryForm } from "./CreateSubcategoryForm";
+import { CategoryForm } from "./CreateCategoryForm";
+import { ProductForm } from "./CreateProductForm";
+import { UserForm } from "./CreateUserForm";
+import {ServiceForm} from "./CreateServiceForm";
+import { PreviousClientForm } from "./CreatePreviousClientForm";
+import { CompanyForm } from "./CreateCompanyForm";
+
+export default {
+    SubcategoryForm,
+    CategoryForm,
+    ProductForm,
+    UserForm,
+    ServiceForm,
+    PreviousClientForm,
+    CompanyForm
+};
---
+++ Web/app/src/Components/Admin/ItemView/ItemView.tsx
@@ -0,0 +1,48 @@
+import classes from "./ItemView.module.scss";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+//still have to decide if I keep ids visible or not
+
+interface Props {
+  item: Item;
+  links: LinkableItem[];
+}
+
+interface Item {
+  [key: string]: string | number;
+}
+
+interface LinkableItem extends Item {
+  name: string;
+  link: string;
+}
+
+//add later for images and stuff
+export const ItemView = (props: Props) => {
+  return (
+    <div className={classes.Container}>
+      {Object.keys(props.item).map((key: string) => {
+        if (props.links.find((item) => item.name === key))
+          return (
+            <div className={classes.Attribute}>
+              <h3>{key}</h3>
+              <Link
+                to={
+                  props.links.find((item) => item.name === key)?.link as string
+                }
+              >
+                {props.item[key]}
+              </Link>
+            </div>
+          );
+        return (
+          <div className={classes.Attribute}>
+            <h3>{key}</h3>
+            <pre>{props.item[key]}</pre>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
