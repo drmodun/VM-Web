1dd563a6de75e209d14c425f006828d6641a22c5
drmodun <jan.modun.st@gmail.com>
2023-07-28 01:08:42 +0200

    added category view

--- frontend/app/src/Pages/Web/Homepage/Homepage.tsx
+++ frontend/app/src/Pages/Web/Homepage/Homepage.tsx
@@ -1,36 +1,48 @@
-import { Product, getProducts } from '../../../Api/ProductApi';
-import ProductView from '../../../Components/Web/ProductView';
-import { SortAttributeType, SortType } from '../../../Types/Enums';
-import classes from './Homepage.module.scss';
-import { useEffect, useState } from 'react';
+import { Category, getCategories } from "../../../Api/CategoryApi";
+import { Product, getProducts } from "../../../Api/ProductApi";
+import CategoryView from "../../../Components/Web/CategoryView";
+import ProductView from "../../../Components/Web/ProductView";
+import { SortAttributeType, SortType } from "../../../Types/Enums";
+import classes from "./Homepage.module.scss";
+import { useEffect, useState } from "react";
 export const Homepage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
 
-    const [products, setProducts] = useState<Product[]>([]);
+  const productFetcher = async () => {
+    const response = await getProducts({
+      "Sorting.Attribute": SortAttributeType.SortByProfit,
+      "Sorting.SortType": SortType.Descending,
+    });
+    setProducts(response?.items!);
+  };
 
-    const productFetcher = async () => {
-        const response = await getProducts(
-            {
-                "Sorting.Attribute": SortAttributeType.SortByProfit,
-                "Sorting.SortType": SortType.Descending,
-            }
-        );
-        setProducts(response?.items!);
-    }
+  const categoryFetcher = async () => {
+    const response = await getCategories({
+      "Sorting.Attribute": SortAttributeType.SortByName,
+      "Sorting.SortType": SortType.Descending,
+    });
+    setCategories(response?.items!);
+  };
 
-    useEffect(() => {
-        productFetcher();
-    }, []);
+  useEffect(() => {
+    productFetcher();
+    categoryFetcher();
+  }, []);
 
-
-    return (
-        <div>
-            <h1>Homepage</h1>
-            <div className={classes.ProductRow}>
-                {products.map((product) => (
-                    <ProductView product={product} />
-                ))}
-            </div>
-        </div>
-    )
-    
-}
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <div className={classes.ProductRow}>
+        {products.map((product) => (
+          <ProductView product={product} />
+        ))}
+      </div>
+      <div className={classes.CategoryRow}>
+        {categories.map((category) => (
+          <CategoryView category={category} isShort={true} />
+        ))}
+      </div>
+    </div>
+  );
+};
